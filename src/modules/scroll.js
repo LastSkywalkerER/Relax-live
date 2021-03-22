d94bba8dfddec654f4b82292f3c4a7a787ae831c
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-22 22:49:05 +0300

    smooth scroll v1.0

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,30 +1,28 @@
 const scroll = () => {
 
   let positionControl = null,
-    speed = 10, // скопрость прокрутки, больше - быстрее
-    accelerate;
+    accelerate,
+    direction = 0;
 
-  const accelerateRatio = 0.5; // ускорение прокрутки от 0 до 1
+  const accelerateRatio = 0.6, // ускорение прокрутки от 0 до 1
+    speed = 10; // скопрость прокрутки, больше - быстрее
 
   const paraboleByThreePointsY = (x1, y1, x2, y2, x3, y3, x, dx) => {
     const a = (y3 - (x3 * (y2 - y1) + x2 * y1 - x1 * y2) / (x2 - x1)) / (x3 * (x3 - x1 - x2) + x1 * x2),
       b = (y2 - y1) / (x2 - x1) - a * (x1 + x2),
       c = (x2 * y1 - x1 * y2) / (x2 - x1) + a * x1 * x2;
-
-    console.log(x1, y1, x2, y2, x3, y3, x, dx);
-
+    // console.log(x1, y1, x2, y2, x3, y3, x, dx, a * x * x + b * x + c);
     return a * x * x + b * x + c + dx;
   };
 
-  const smoothScroll = scrollTo => {
-    const positionY = document.querySelector(scrollTo).offsetTop,
-      position = document.documentElement.scrollTop;
+  const smoothScroll = (scrollFrom, scrollTo) => {
+    const position = document.documentElement.scrollTop;
 
-    document.documentElement.scrollTop = position + paraboleByThreePointsY(Math.min(0, positionY), 0, positionY / 2, accelerate, Math.max(0, positionY), 0, position, speed);
-
+    document.documentElement.scrollTop = position + direction * paraboleByThreePointsY(Math.min(scrollFrom, scrollTo), 0, Math.abs(Math.max(scrollFrom, scrollTo) - Math.min(scrollFrom, scrollTo)) / 2, accelerate, Math.max(scrollFrom, scrollTo), 0, position, speed);
+    console.log(scrollFrom, scrollTo);
     if (positionControl !== position) {
       requestAnimationFrame(() => {
-        smoothScroll(scrollTo);
+        smoothScroll(scrollFrom, scrollTo);
       });
     }
     positionControl = position;
@@ -37,6 +35,7 @@ const scroll = () => {
       link = link.getAttribute('href');
       if (link[0] === '#' &&
         link !== '#close' && link.length > 1) {
+        event.preventDefault();
 
         const positionY = document.querySelector(link).offsetTop,
           startPoint = document.documentElement.scrollTop;
@@ -46,13 +45,11 @@ const scroll = () => {
         accelerate = Math.pow(Math.max(positionY, startPoint) - Math.min(positionY, startPoint), accelerateRatio);
 
         if (positionY >= startPoint) {
-          speed = Math.abs(speed);
+          direction = 1;
         } else {
-          speed = -Math.abs(speed);
+          direction = -1;
         }
-
-        event.preventDefault();
-        smoothScroll(link);
+        smoothScroll(startPoint, positionY);
 
       }
     }
