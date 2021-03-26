598001a9399533a5b928da870b3cdf2c3d6bf6d8
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-26 11:46:48 +0300

    add clode accordion and overflow hidden

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -8,25 +8,36 @@ const accordeon = ({
     items = [...wrap.querySelectorAll(itemSelector)],
     messages = [...wrap.querySelectorAll(messageSelector)];
 
-  let prevIndex = 0;
+  let prevIndex = 0,
+    isOpen = 1;
 
   const closeAccordion = (index) => {
     items[index].classList.remove(activeSelector.slice(1));
+    isOpen = 0;
   };
 
   const openAccordion = (index) => {
     items[index].classList.add(activeSelector.slice(1));
-  }
+    isOpen = 1;
+  };
 
   wrap.addEventListener('click', event => {
     items.forEach((item, index) => {
       if (item === event.target || item.contains(event.target)) {
-        closeAccordion(prevIndex);
+        console.log(isOpen);
         if (prevIndex !== index) {
+          closeAccordion(prevIndex);
           openAccordion(index);
+        } else {
+          if (isOpen) {
+            closeAccordion(prevIndex);
+          } else {
+            openAccordion(index);
+          }
         }
         prevIndex = index;
       }
+
     });
   });
 };
