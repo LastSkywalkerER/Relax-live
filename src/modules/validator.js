34903f90d315bb67c96e11c3b086d3fb0841da2b
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-27 12:21:19 +0300

    make validator more beautiful

--- src/modules/validator.js
+++ src/modules/validator.js
@@ -23,13 +23,39 @@ class Validator {
     });
     this.error = new Set();
     this.form.setAttribute('novalidate', '');
-    this.submitBtn.setAttribute('disabled', '');
+    this.hideButton = document.createElement('div');
+    this.buttonWrap = document.createElement('div');
+    this.buttonWrap.classList.add('validator-disabled');
   }
 
   init() {
     this.applyStyle();
     this.setPattern();
-    this.elementsForm.forEach(elem => elem.addEventListener('input', this.checkIt.bind(this)));
+    this.disableButton();
+    this.elementsForm.forEach(elem => elem.addEventListener('input', () => {
+      this.elementsForm.forEach(target => {
+        if (this.isValid(target)) {
+          this.error.delete(target);
+          if (!this.error.size) {
+            this.enableButton();
+          }
+        } else {
+          this.error.add(target);
+          if (this.error.size) {
+            this.disableButton();
+          }
+        }
+      });
+    }));
+    this.form.addEventListener('click', event => {
+      if (event.target === this.hideButton) {
+        this.checkIt();
+      } else {
+        this.elementsForm.forEach(target => {
+          this.showSuccess(target);
+        });
+      }
+    });
     this.form.addEventListener('submit', e => {
       this.elementsForm.forEach(elem => this.checkIt({
         target: elem
@@ -37,7 +63,7 @@ class Validator {
       e.preventDefault();
 
       if (this.error.size) {
-        this.submitBtn.setAttribute('disabled', '');
+        this.disableButton();
       }
     });
   }
@@ -73,19 +99,35 @@ class Validator {
     return true;
   }
 
+  disableButton() {
+    this.submitBtn.setAttribute('disabled', '');
+    this.submitBtn.classList.add('disabled');
+    this.submitBtn.insertAdjacentElement('afterend', this.buttonWrap);
+    this.buttonWrap.append(this.submitBtn);
+    this.buttonWrap.append(this.hideButton);
+  }
+
+  enableButton() {
+    this.submitBtn.removeAttribute('disabled', '');
+    this.submitBtn.classList.remove('disabled');
+    this.buttonWrap.insertAdjacentElement('afterend', this.submitBtn);
+    this.hideButton.remove();
+    this.buttonWrap.remove();
+  }
+
   checkIt(event) {
     this.elementsForm.forEach(target => {
       if (this.isValid(target)) {
         this.showSuccess(target);
         this.error.delete(target);
         if (!this.error.size) {
-          this.form.querySelector('button').removeAttribute('disabled', '');
+          this.enableButton();
         }
       } else {
         this.showError(target);
         this.error.add(target);
         if (this.error.size) {
-          this.form.querySelector('button').setAttribute('disabled', '');
+          this.disableButton();
         }
       }
     });
@@ -115,15 +157,15 @@ class Validator {
     const style = document.createElement('style');
     style.textContent = `
       input.success {
-        border: 2 px solid green !important;
-        color: green !important;
+        border: 2 px solid #7ce319 !important;
+        color: #7ce319 !important;
       }
       input.error {
-        border: 2 px solid red !important;
-        color: red !important;
+        border: 2 px solid #E32636 !important;
+        color: #E32636 !important;
       }
       .validator-error {
-        color: red;
+        color: #E32636;
         position: absolute;
         top: 0;
         transform: translateY(-75%);
@@ -131,6 +173,17 @@ class Validator {
         background: white;
         border-radius: 10px;
 
+      }
+      .validator-disabled {
+        position: relative;
+      }
+      
+      .validator-disabled > div {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
       }
     `;
 
